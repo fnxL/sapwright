97f1626ef90d5e0e5ad4dcf82c5a76bced0957ad
fnxL <48607132+fnxL@users.noreply.github.com>
2026-09-23 22:49:13 +0530

    feat: add custom sap exceptions

--- src/sapwright/exceptions.py
+++ src/sapwright/exceptions.py
@@ -4,3 +4,43 @@ class SAPGuiError(Exception):
 
 class SAPLogonError(SAPGuiError):
     """Raised when SAP Logon fails to launch"""
+
+
+class SAPConnectionError(SAPGuiError):
+    """Raised when a connection to SAP Logon fails"""
+
+
+class SAPScriptingDisabled(SAPGuiError):
+    """Raised when SAP GUI Scripting is disabled by server"""
+
+
+class SAPElementNotFound(SAPGuiError):
+    """Raised when a SAP GUI element is not found"""
+
+
+class SAPElementNotChangeable(SAPGuiError):
+    """Raised when attempting to modify a read-only element."""
+
+
+class SAPComboBoxOptionNotFound(SAPGuiError):
+    """Raised when a specified option is not available in a ComboBox."""
+
+
+class SAPStatusBarError(SAPGuiError):
+    """Raised when there is an error in the status bar."""
+
+
+class SAPTransactionError(SAPGuiError):
+    """Raised when a transaction fails to start or encounters a critical error."""
+
+
+class SAPLoginError(SAPGuiError):
+    """Raised when the login process fails."""
+
+
+class SAPElementTypeMismatch(SAPGuiError):
+    """Raised when the element type does not match the expected type."""
+
+
+class SAPTableConfigurationError(SAPGuiError):
+    """Raised when there is a mismatch or error in table configuration/population."""
